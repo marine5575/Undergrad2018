99064a939e77e1d6a2ca5208db9858c1b495fd42
Jiwon Park <minerva1993@gmail.com>
2019-05-09 17:28:32 +0900

    keep eventinfo

--- mva/mkNtuple/launchAna.py
+++ mva/mkNtuple/launchAna.py
@@ -1,7 +1,17 @@
 from subprocess import call
 import sys
+from ROOT import TFile
 
 file_path = sys.argv[1]
 file_name = sys.argv[2]
 
 call(["root", "-l", 'run.C("' + file_path + '", "' + file_name + '")'], shell=False)
+
+## save Event Summary histogram ##
+f = TFile('./../../analyzer/output/hist_'+file_path.split('/')[-1].replace('sample_',''), "read")
+out = TFile(file_name, "update")
+hevt = f.Get("EventInfo")
+out.cd()
+hevt.Write()
+out.Write()
+out.Close()
